17e0bae4a57f002d853f7c8f24a3a05559f5d85c
viktoronvudive <1032215834@pfur.ru>
2023-02-25 06:24:06 -0800

    add lab

---
+++ labs/lab07/calc.asm
@@ -0,0 +1,30 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите X ',0
+rem: DB 'выражение = : ',0
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax, msg
+call sprintLF
+mov ecx, x
+mov edx, 80
+call sread
+mov eax,x ; вызов подпрограммы преобразования
+call atoi ; ASCII кода в число, `eax=x`
+
+add eax,5
+mov ebx,eax
+mul ebx
+sub eax,3
+
+mov ebx,eax
+mov eax,rem
+call sprint
+mov eax,ebx
+call iprintLF
+call quit
+
+
